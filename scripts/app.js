b6213d0aeda5afeebda9d01bc6cfc4331ee0d7bd
wisniewskiz <wisniewskiz@live.com>
2020-12-26 15:23:02 -0600

    fix image path issues

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 let portrait = new hoverEffect({
     parent: document.querySelector('.cta--image'),
     intensity: 0.5,
-    image1: '../assets/blurred.jpg',
-    image2: '../assets/blurred.jpg',
-    displacementImage: '../assets/heightMap.png',
+    image1: './assets/blurred.jpg',
+    image2: './assets/blurred.jpg',
+    displacementImage: './assets/heightMap.png',
     angle: Math.PI / 8
 });
